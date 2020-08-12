11dfbf0f0edd8e328a7a9de65f9a843945e44a7f
FuZhao1001 <52687874+FuZhao1001@users.noreply.github.com>
2020-08-12 11:41:30 +0800

    测试2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I love java!!!");
+        System.out.println("小测试");
 	// write your code here
     }
 }
